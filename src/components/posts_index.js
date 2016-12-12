8b333e491c808661153f4ac9c5e7aba546378178
bud aminof <budaminof@gmail.com>
2016-12-12 12:57:30 -0700

    new route for forms

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index';
 
-export default () => {
-  return (
-    <div>List of blog posts:</div>
-  );
-};
+class PostsIndex extends Component {
+  // life cycle method
+  // react will pull this method when ever a componenet is about to be
+  // render for the first time. called only once.
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return (
+      <div>List of blog posts</div>
+    );
+  }
+}
+
+// a shortcut for the mapDispatchToProps and bindActionCreators =>
+// { fetchPosts: fetchPosts } just pass it as an object to PostsIndex;
+export default connect(null, { fetchPosts })(PostsIndex);
+// null is instead of mapStateToProps
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import { Route, IndexRoute} from 'react-router';
 
 import App from './components/app';
 import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 
 // Greeting is a child component of App and is being passed to App
 // as this.props.children
 export default (
   <Route path="/" component={App} >
     <IndexRoute component={PostsIndex} />
+    <Route path="posts/new" component={PostsNew} />
   </Route>
 );
