a4e12e356939820c2709c8b560a2cc518e3203cb
bud aminof <budaminof@gmail.com>
2016-12-12 16:43:22 -0700

    redirecting to / after submission

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,15 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
 import { Link } from 'react-router';
 
 class PostsNew extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
+  onSubmit(props) {
+    this.props.createPost(props)
+      .then(()=> {
+        // blog post has been created navigate user to the index
+        // we navigate by calling this.context.router.push with the new path
+        this.context.router.push('/');
+       })
+  }
+
   render() {
     // handleSubmit is a function handed to us from redux-form
     const { fields: { title, categories, content }, handleSubmit } = this.props;
 
     return(
-      <form onSubmit={ handleSubmit(this.props.createPost) } >
+      <form onSubmit={ handleSubmit(this.onSubmit.bind(this)) } >
         <h3>Create A New Post</h3>
 
         <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
