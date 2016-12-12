7e3bbd67792de0feb5d58a18d299566972cdf0d2
bud aminof <budaminof@gmail.com>
2016-12-12 11:43:19 -0700

    action creator

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export const FETCH_POSTS = "FETCH_POSTS";
+
+const ROOT_UTL = 'http://reduxblog.herokuapp.com/api';
+const API_KEY = '?key=tinytinytinytinytinytinytiny';
+
+export function fetchPosts() {
+  const request = axios.get(`${ROOT_UTL}/posts/${API_KEY}`);
+
+  return {
+    type: FETCH_POSTS,
+    payload: request
+  };
+};
