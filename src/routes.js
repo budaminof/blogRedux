cdcc7c2c1ca907182cd0796b254461712ec7ca34
bud aminof <budaminof@gmail.com>
2016-12-12 17:12:46 -0700

    navigate to a single post from the list

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ import { Route, IndexRoute} from 'react-router';
 import App from './components/app';
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
+
 
 // Greeting is a child component of App and is being passed to App
 // as this.props.children
@@ -11,5 +13,8 @@ export default (
   <Route path="/" component={App} >
     <IndexRoute component={PostsIndex} />
     <Route path="posts/new" component={PostsNew} />
+    <Route path="posts/:id" component={PostsShow} />
   </Route>
 );
+
+// this.props.params.id
